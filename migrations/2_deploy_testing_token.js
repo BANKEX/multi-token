c75c1d56ac51c36ea71ebe47ebfacce3ba8764c7
Igor Gulamov <igor.gulamov@gmail.com>
2018-05-14 03:26:19 +0300

    documentation fix

---
+++ migrations/2_deploy_testing_token.js
@@ -0,0 +1,14 @@
+var TestingToken = artifacts.require("./TestingToken.sol");
+
+
+
+module.exports = function(deployer, network, accounts) {
+  const operator = accounts[0];
+  (async () => {
+    await deployer.deploy(TestingToken, {"from" : operator});
+    let testingToken = await TestingToken.deployed();
+
+  })();
+
+  
+};
